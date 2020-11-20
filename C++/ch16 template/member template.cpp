c9ca99f3c8164f425176a3c216a27816568bf348
kingtub <2044372796@qq.com>
2020-11-20 11:57:13 +0800

    优化

--- C++/ch16 template/member template.cpp
+++ C++/ch16 template/member template.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -63,5 +64,8 @@ void test2()
 {
 	// 编译器根据 函数模板的实参来推断它的实参类型； 
 	B<int> b(5.0);
-	cout<<b.smaller("hhu", "kkk")<<endl;
+	// 将实参传递给带模板类型的函数形参时，
+	// 能够自动应用的类型转换只有const转换及数组或函数到指针的转换。 
+	cout<<b.smaller("hhu", "kkk")<<endl; // 字符指针地址的大小比较，有歧义？ 
+	cout<<b.smaller(string("hhu"), string("kkk"))<<endl; 
 }
