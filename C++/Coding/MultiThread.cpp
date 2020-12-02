2e17660bb40ce14e0846d876d57603e54077c6cb
kingtub <2044372796@qq.com>
2020-12-02 15:59:25 +0800

    多线程

--- C++/Coding/MultiThread.cpp
+++ C++/Coding/MultiThread.cpp
@@ -9,7 +9,7 @@ void aa()
 {
 	for(int i = 0; i < 100; i++) {
 		cout<<"Thread aa out "<<i<<" "<<endl;
-		this_thread::sleep_for(chrono::milliseconds(1));
+		this_thread::sleep_for(chrono::milliseconds(100));
 	}
 	cout<<"Thread aa finished"<<endl;
 } 
@@ -18,7 +18,7 @@ void bb()
 {
 	for(int i = 0; i < 100; i++) {
 		cout<<"Thread bb out "<<i<<" "<<endl;
-	    this_thread::sleep_for(chrono::milliseconds(1));
+	    this_thread::sleep_for(chrono::milliseconds(100));
 	}
 	cout<<"Thread bb finished"<<endl;
 } 
@@ -27,7 +27,9 @@ void test1()
 {
 	thread t1(aa);
 	thread t2(bb);
-	this_thread::sleep_for(chrono::milliseconds(1000));
+	//this_thread::sleep_for(chrono::milliseconds(1000));
+	t1.join();
+	t2.join();
 }
 
 int main()
